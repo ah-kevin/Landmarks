ede953e255a5872f838577ac42053a0763a9fed1
bjke <kbjwdh@gmail.com>
2023-05-23 23:22:04 +0800

    handling-user-input

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -5,9 +5,12 @@
 //  Created by bjke on 2023/5/23.
 //
 
+import Combine
 import Foundation
+final class ModelData: ObservableObject {
+  @Published var landmarks: [Landmark] = load("landmarkData.json")
+}
 
-var landmarks: [Landmark] = load("landmarkData.json")
 // 创建一个 load(_:) 方法，从应用程序的主要捆绑包中获取给定名称的 JSON 数据。
 // 加载方法依赖于返回类型符合 Decodable 协议，这是 Codable 协议的一个组成部分。
 func load<T: Decodable>(_ filename: String) -> T {
