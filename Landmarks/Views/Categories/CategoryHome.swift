bb41c636061b294f517775a6490692b31dafa998
bjke <kbjwdh@gmail.com>
2023-05-25 10:29:52 +0800

    1

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by bjke on 2023/5/25.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
